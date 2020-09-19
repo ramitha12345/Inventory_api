9397c4c85d375d41f0bd70d93904d692325fa543
Pathum Kalhan <pathumsimpson@gmail.com>
2020-09-19 09:42:18 +0530

    report

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -1,30 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const checkAuth = require('../middlewares/checkAuth');
-const db = require('../../models');
+const checkAuth = require("../middlewares/checkAuth");
+const db = require("../../models");
 
-router.post('/reorderLevel', async (req, res) => {
-    try {
-        const orderBy = req.body.orderBy;
-        const status = req.body.status;
-        const query = `SELECT 
+router.post("/reorderLevel", async (req, res) => {
+  try {
+    const orderBy = req.body.orderBy;
+    const status = req.body.status;
+    const query = `SELECT 
        *
    FROM
        inventory.products
    WHERE
-       qty < reorderLevel ${status === 'all' ? "" : `AND status =${status}`}
-   ORDER BY ${orderBy};`
+       qty < reorderLevel ${status === "all" ? "" : `AND status =${status}`}
+   ORDER BY ${orderBy};`;
 
-        const data = await db.sequelize.query(query,
-            {
-                type: db.sequelize.QueryTypes.SELECT,
-            });
-        res.status(200).json(data);
-    } catch (error) {
-        res.sendStatus(500);
-    }
+    const data = await db.sequelize.query(query, {
+      type: db.sequelize.QueryTypes.SELECT,
+    });
+    res.status(200).json(data);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
 
+router.post("/category", async (req, res) => {
+  try {
+    const orderBy = req.body.orderBy;
+    const status = req.body.status;
+    const date = req.body.date;
 
+    const query = `SELECT * FROM inventory.categories
+WHERE
+    DATE(createdAt) >= DATE('${date}')
+    ${status === "all" ? "" : `AND status =${status}`}
+ORDER BY ${orderBy};`;
+
+    const data = await db.sequelize.query(query, {
+      type: db.sequelize.QueryTypes.SELECT,
+      logging:console.log
+    });
+    
+    res.status(200).json(data);
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
